0eccac3207c7de62cc787a1d7c86448eb2444982
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2024-04-03 23:38:59 +0300

    Created claims folder with extensions for System.Security.Claims library, manipulating identity data work.

---
+++ FlowerLovers.Core/Claims/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Security.Claims
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        // Return the id of the currentUser.
+        public static string UserId(this ClaimsPrincipal claim) 
+        {
+            return claim.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
