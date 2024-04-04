4b585819e144de942d3b8cd983bcf241ff461eb8
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2024-04-04 17:51:35 +0300

    Removed unnecessary source code from program.cs class, which was added through the scaffolding.

--- FlowerLovers/Program.cs
+++ FlowerLovers/Program.cs
@@ -1,14 +1,4 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using FlowerLovers.Data.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("FlowerLoversDbContextConnection") ?? throw new InvalidOperationException("Connection string 'FlowerLoversDbContextConnection' not found.");
-
-builder.Services.AddDbContext<FlowerLoversDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<FlowerLoversDbContext>();
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity();
