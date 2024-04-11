38ad2edf516f3d59ea253ba454f4363436664bfb
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2024-04-12 01:57:00 +0300

    Created application service user, implementing the application service user interface, with method for setting user first name and last name into full name.

---
+++ FlowerLovers.Core/Services/ApplicationUser/ApplicationServiceUser.cs
@@ -0,0 +1,43 @@
+﻿using FlowerLovers.Core.Contracts;
+using FlowerLovers.Core.CustomExceptions;
+using FlowerLovers.Data.Data;
+using static FlowerLovers.Core.CustomExceptions.DataConstants.UserDataConstants;
+using static FlowerLovers.Core.CustomExceptions.DataConstants.NameDataConstants;
+
+namespace FlowerLovers.Core.Services.ApplicationUser
+{
+    public class ApplicationServiceUser : IApplicationServiceUser
+    {
+        private readonly FlowerLoversDbContext data;
+
+        public ApplicationServiceUser(FlowerLoversDbContext _data)
+        {
+            data = _data;
+        }
+
+        public async Task<string> UserFullNameAsync(string userId)
+        {
+            if (userId == null)
+            {
+                throw new ArgumentNullException(nameof(userId));
+            }
+            var user = await data.Users.FindAsync(userId);
+
+            if (user == null)
+            {
+                throw new UserNullException(USERNULLMESSAGE);
+            }
+
+            if (string.IsNullOrEmpty(user.FirstName)
+                ||
+                string.IsNullOrEmpty(user.LastName)
+                )
+            {
+                // Create a custom exception.
+                throw new NameNullOrEmptyException(NAMEERRORMESSAGE);
+            }
+
+            return user.FirstName + " " + user.LastName;
+        }
+    }
+}
