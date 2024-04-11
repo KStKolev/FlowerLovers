328b8979e23899e7b851a237db85306ee7f570ca
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2024-04-12 01:49:25 +0300

    Created Register controller with functionality for view model for introducement the user after the registration.

---
+++ FlowerLovers/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using FlowerLovers.Core.Contracts;
+using FlowerLovers.Core.Services.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FlowerLovers.Web.Controllers
+{
+    [Authorize]
+    public class RegisterController : Controller
+    {
+        private readonly IApplicationServiceUser userService;
+
+        public RegisterController(IApplicationServiceUser _userService)
+        {
+            userService = _userService;
+        }
+
+        public async Task<IActionResult> Introducement() 
+        {
+            var userId = User.UserId();
+
+            ApplicationUserModel model = new ApplicationUserModel();
+            model.FullName = await userService.UserFullNameAsync(userId);
+
+            return View(model);
+        }
+    }
+}
